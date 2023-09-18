8d5e44829ef99bf6db7826e3118de8b06917f25e
Thurow <luana.thurow@dellteam.com>
2023-09-17 21:10:19 -0300

    utilizando Props com vue-ts

---
+++ vue/demo-vueProps/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AloMundo from './components/AloMundo.vue';
+import AloMundo2 from './components/AloMundo2.vue';
+import AloMundo3 from './components/AloMundo3.vue';
+</script>
+
+<template>
+  <!--
+  <AloMundo nome="Luana" />
+  <AloMundo />
+  <AloMundo2 nome="Luana Maria" />-->
+  <AloMundo3 nome="Luana" :repeticao="3" />
+  <AloMundo3 nome="Luana Maria" />
+</template>
+
+<style scoped></style>
